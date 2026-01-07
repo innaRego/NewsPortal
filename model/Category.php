7838c1cbf5a3fd7df4867ad4ccf19f128c8a1564
Inna Rego <inna.rego@ivkhk.ee>
2026-01-07 18:55:55 +0200

    главная страница и страница новостей

---
+++ model/Category.php
@@ -0,0 +1,11 @@
+<?php
+
+class Category {
+
+    public static function getAllCategory() {
+        $query = "SELECT * FROM category";
+        $db = new Database();
+        return $db->getAll($query);
+    }
+
+}
---
+++ view/start.php
@@ -0,0 +1,10 @@
+<?php
+ob_start();
+?>
+<h1>TOP 3 News</h1>
+<br>
+<?php
+ViewNews::NewsByCategory($arr);
+$content = ob_get_clean();
+include_once 'view/layout.php';
+?>
